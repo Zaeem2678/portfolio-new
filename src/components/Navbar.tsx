ae2c9c0c66219abeaa78cfc5353f1e57e32b1fc5
Zaeem2678 <102031010+Zaeem2678@users.noreply.github.com>
2025-10-30 15:32:09 +0300

    finalised

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,10 +35,9 @@ const Navbar = () => {
         <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
             {/* Logo */}
-            <Link href="/" className="flex items-center space-x-3">
-              <Code2 className="w-8 h-8 text-white" />
-              <span className="text-xl font-bold text-white">Zaeem</span>
-            </Link>
+
+            <Code2 className="w-8 h-8 text-white" />
+            <span className="text-xl font-bold text-white">Zaeem</span>
 
             {/* Desktop Navigation */}
             <div className="hidden md:flex items-center space-x-6">
